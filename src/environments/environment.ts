75c42acdd2c41b972b8cb3310b89f7289394bc95
GuilhermeK <guilhermekscampos@gmail.com>
2024-12-24 13:40:38 -0300

    att

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8000/api/',
+//   validateEmailUrl: 'http://localhost:8000/api/validate-email/',
+// };
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api/',
-  validateEmailUrl: 'http://localhost:8000/api/validate-email/',
+  production: false, // ou true no ambiente de produção
+  apiUrl: 'https://www.licitacao360.com/api/', // Certifique-se de que termina com '/'
+  validateEmailUrl: 'https://www.licitacao360.com/api/validate-email/',
 };
